ca50e26055bbcc3d7efa269ea27ee66628ecac9d
wildstoats <get@thewildstoat.com>
2020-12-25 01:27:24 -0500

    Stay try to Koa idioms for async functions and middleware

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,11 +1,11 @@
-// const Koa = require('koa');
 import Koa from 'koa';
 const app = new Koa();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
-app.use(async ctx => {
+app.use(async (ctx, next) => {
   ctx.body = 'Hello World';
+  await next();
 });
 
 app.listen(PORT);
