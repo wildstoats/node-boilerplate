c2f9a2d5576fea7dfc8712b688988caa63004ae5
wildstoats <get@thewildstoat.com>
2020-12-30 15:32:46 -0500

    Add baseline frontend config

---
+++ frontend/src/Root.js
@@ -0,0 +1,7 @@
+import {hot} from 'react-hot-loader/root';
+
+import AppShell from './components/AppShell';
+
+import './styles.scss';
+
+export default hot(AppShell);
---
+++ frontend/src/components/AppShell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AppShell() {
+  return (
+    <div className="app-shell">
+      Node Boilerplate Frontend
+    </div>
+  );
+};
